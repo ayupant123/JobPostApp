c88f42029ee2b449dd0244304a3e3c1687326b5c
Ayushi Pant <ayupant@publicisgroupe.net>
2022-11-18 15:32:12 +0530

    initial commit

---
+++ src/main/java/com/company/joblistings/JobListingsApplication.java
@@ -0,0 +1,13 @@
+package com.company.joblistings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobListingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobListingsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/joblistings/JobListingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.joblistings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobListingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
